3f01fb62029bd462476ccbd31cc170aedf284240
Snowyfall0212 <mrtysonvolk@gmail.com>
2025-11-27 12:08:10 -0700

    changes

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 const postNames = document.querySelectorAll(".post-name")
 console.log(postNames)
+
+
+
+const dropdown = document.getElementById('dropdown');
+
+dropdown.addEventListener('change', function() {
+    console.log(this)
+})
