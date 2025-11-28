9e80858c475c03eb5c596bc3aa3315adfc9f2db9
Snowyfall0212 <mrtysonvolk@gmail.com>
2025-11-27 18:08:43 -0700

    index js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,31 @@
 const postNames = document.querySelectorAll(".post-name")
 console.log(postNames)
 
+let votes = {
+    Mona: 4,
+    Tenley: 6,
+    Maxwell: 7,
+}
 
+const monaVotes = document.getElementById("mona-votes")
+const tenleyVotes = document.getElementById("tenley-votes")
+const maxwellVotes = document.getElementById("maxwell-votes")
 
-const dropdown = document.getElementById('dropdown');
+let pick = ""
 
-dropdown.addEventListener('change', function() {
-    console.log(this)
+const dropdown = document.getElementById('dropdown')
+dropdown.addEventListener('change', function() {    
+    Array.from(this.selectedOptions).forEach(option => {
+        let newPick = option.value
+
+        if (newPick === pick) { // really this isnt necessary but i did this anyway to make it clearer for me
+            console.log("nothing changed.")
+        } else if (newPick !== pick) {
+            votes[newPick] += 1
+            votes[pick] -= 1
+        }
+
+        console.log(votes)
+        pick = newPick
+    })
 })
