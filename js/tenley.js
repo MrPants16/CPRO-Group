71769665c24cd5c044c8707d54762f37154ec5c4
Snowyfall0212 <mrtysonvolk@gmail.com>
2025-11-27 22:23:03 -0700

    hopefully the last major changes to my tenley

--- js/tenley.js
+++
@@ -1,7 +0,0 @@
-const heartButton = document.getElementById("heart-button")
-let counter = 1
-
-heartButton.addEventListener('click', function() {
-    counter += 1
-    heartButton.textContent = `🤍 ${counter}`
-})
